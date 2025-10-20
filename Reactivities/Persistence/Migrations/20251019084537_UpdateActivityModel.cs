174e4ef16c4c7b226571042f48940ec80bae5a9d
SONG <songx9241@gmail.com>
2025-10-20 16:04:44 +0900

    End of section 2

---
+++ Reactivities/Persistence/Migrations/20251019084537_UpdateActivityModel.cs
@@ -0,0 +1,62 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class UpdateActivityModel : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "City",
+                table: "Activities",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<double>(
+                name: "Latitude",
+                table: "Activities",
+                type: "REAL",
+                nullable: false,
+                defaultValue: 0.0);
+
+            migrationBuilder.AddColumn<double>(
+                name: "Longitude",
+                table: "Activities",
+                type: "REAL",
+                nullable: false,
+                defaultValue: 0.0);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Venue",
+                table: "Activities",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "City",
+                table: "Activities");
+
+            migrationBuilder.DropColumn(
+                name: "Latitude",
+                table: "Activities");
+
+            migrationBuilder.DropColumn(
+                name: "Longitude",
+                table: "Activities");
+
+            migrationBuilder.DropColumn(
+                name: "Venue",
+                table: "Activities");
+        }
+    }
+}
